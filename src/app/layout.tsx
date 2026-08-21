80a449070145188f33677a592b220c9b9503f592
Deimantas <deimas.0d@gmail.com>
2026-08-21 17:36:51 -0500

    Add Google Analytics GA4 (G-X3N5LB8WFK)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,6 +109,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-X3N5LB8WFK"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-X3N5LB8WFK');`,
+          }}
+        />
+        {/* End Google Analytics */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <meta name="geo.region" content="US-AR" />
